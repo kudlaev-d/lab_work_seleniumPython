5ee85549ca8cb7e02be7ceb5b30e89f60c9a969d
Denis Kudlaev <denis.kudlaev@ironwaterstudio.com>
2022-05-28 20:22:59 +0300

    Доработки по 2-му заданию

--- pageobjects/search_page.py
+++ pageobjects/search_page.py
@@ -35,21 +35,31 @@ class SearchPage(BasePage):
         return self.driver.find_element(By.CLASS_NAME, 'input-group-btn')
 
     def get_notification(self) -> str:
+        """Получения нотификации при пустом результате поиска"""
         return self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div/p[2]').text
 
     def clean_search_field(self):
         self.get_search_field().clear()
 
+    def clean_search_criteria_field(self):
+        self.get_search_criteria_field().clear()
+
     def input_search_criteria_keywords(self, keywords: str):
+        """Ввод доп. критериев"""
         self.get_search_criteria_field().send_keys(keywords)
 
     def search_by_criteria(self, criteria: str):
-        self.get_button_search_criteria().send_keys(criteria)
+        """Поиск по доп. критериям"""
+        self.get_search_criteria_field().send_keys(criteria)
         self.get_button_search_criteria().click()
 
     def search_by_name(self, name: str):
+        """Глобальный поиск"""
         self.get_search_field().send_keys(name)
         self.get_button_global_search().click()
+
+    def search_in_description_checkbox(self):
+        self.driver.find_element(By.ID, 'description').click()
 
     def get_decimal_price_from_str(self, price_str: str) -> Decimal:
         """Метод извлечения цены и перевода в decimal"""
@@ -61,7 +71,7 @@ class SearchPage(BasePage):
     def get_search_results(self) -> List[ProductInfo]:
         """Возвращает список найденных моделей"""
         products: List[ProductInfo] = []
-        results_tags: List[WebElement] = self.driver.find_elements(By.CLASS_NAME, 'product-thumb')
+        results_tags: List[WebElement] = self.driver.find_elements(By.CLASS_NAME, 'product-layout')
 
         for result_tag in results_tags:
             name: str = self.driver.find_element(By.TAG_NAME, 'h4').text
--- test_search.py
+++ test_search.py
@@ -1,15 +1,11 @@
 import unittest
 from typing import List
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from pageobjects.product_page import ProductPage
 from pageobjects.search_page import SearchPage, ProductInfo
 
-
 class SearchPageTest(unittest.TestCase):
-
     def setUp(self) -> None:
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         self.search_page = SearchPage(self.driver)
@@ -17,19 +13,22 @@ class SearchPageTest(unittest.TestCase):
         self.first_product_name: str = 'Apple'
         self.second_product_name: str = 'Sony'
         self.third_product_name: str = 'nokia'
+        self.criteria_search_text: str = 'stunning'
         self.notification: str = 'There is no product that matches the search criteria.'
 
     def tearDown(self) -> None:
-        self.driver.close()
+        self.driver.quit()
 
     def test_search(self):
         """Проверка работы функции поиска"""
+        # Поиск первого продукта
         self.search_page.search_by_name(self.first_product_name)
         product: List[ProductInfo] = self.search_page.get_search_results()
 
         self.assertEqual(product[0].name, 'Apple Cinema 30"')
         self.assertEqual(product[0].price, 110.00)
 
+        # Поиск второго продукта
         self.search_page.clean_search_field()
         self.search_page.search_by_name(self.second_product_name)
         product: List[ProductInfo] = self.search_page.get_search_results()
@@ -37,9 +36,18 @@ class SearchPageTest(unittest.TestCase):
         self.assertEqual(product[0].name, 'Sony VAIO')
         self.assertEqual(product[0].price, 1202.00)
 
+        # Поиск третьего продукта, которого нет на сайте
         self.search_page.clean_search_field()
         self.search_page.search_by_name(self.third_product_name)
 
         self.assertEqual(self.notification, self.search_page.get_notification())
 
+        # Поиск по доп. критериям
+        self.search_page.clean_search_criteria_field()
+        self.search_page.search_in_description_checkbox()
+        self.search_page.search_by_criteria(self.criteria_search_text)
 
+        product: List[ProductInfo] = self.search_page.get_search_results()
+
+        self.assertEqual(product[0].name, 'HP LP3065')
+        self.assertEqual(product[1].name, 'iMac')

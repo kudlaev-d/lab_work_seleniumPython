07a05722a83b8a987ee59cd179b472483a134931
Denis Kudlaev <denis.kudlaev@ironwaterstudio.com>
2022-05-29 14:26:13 +0300

    adding const

--- test_add_review.py
+++ test_add_review.py
@@ -1,4 +1,6 @@
 import unittest
+from typing import Final, List
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -7,6 +9,9 @@ from random import randrange
 
 
 class AddReviewTest(unittest.TestCase):
+
+    MAX_RATING: Final = 5
+
     def setUp(self) -> None:
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         self.product_page = ProductPage(self.driver)
@@ -16,6 +21,7 @@ class AddReviewTest(unittest.TestCase):
         self.expected_successful_review_alert: str = 'Thank you for your review. ' \
                                                      'It has been submitted to the webmaster for approval.'
         self.name: str = 'John'
+        self.wrong_review_length: int = 24
 
     def tearDown(self) -> None:
         self.driver.quit()
@@ -30,14 +36,14 @@ class AddReviewTest(unittest.TestCase):
         self.assertTrue(self.product_page.is_presence_alert_text(self.expected_rating_alert))
 
         # Выбрать любой рейтинг, ввести имя и комментарий в 24 символа
-        self.product_page.select_rating_value(randrange(5))
+        self.product_page.select_rating_value(randrange(AddReviewTest.MAX_RATING))
         self.product_page.input_name(self.name)
-        self.product_page.input_review(self.product_page.generate_random_string(24))
+        self.product_page.input_review(self.product_page.generate_random_string(self.wrong_review_length))
         self.product_page.send_review()
 
         self.assertTrue(self.product_page.is_presence_alert_text(self.expected_unsuccessful_review_alert))
 
-        # Ввести в поле комментария больше 25 символов
+        # Ввести в поле комментария больше 25 символов (25-1000)
         self.product_page.clear_review_field()
         self.product_page.input_review(self.product_page.generate_random_string(randrange(25, 1001)))
         self.product_page.send_review()

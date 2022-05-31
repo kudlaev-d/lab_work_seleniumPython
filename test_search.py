e3fef25f8c22283b785d7080d8be97be2db87a0c
Denis Kudlaev <denis.kudlaev@ironwaterstudio.com>
2022-05-31 22:46:34 +0300

    Refactoring test_search

--- test_search.py
+++ test_search.py
@@ -14,33 +14,35 @@ class SearchPageTest(unittest.TestCase):
         self.second_product_name: str = 'Sony'
         self.third_product_name: str = 'nokia'
         self.criteria_search_text: str = 'stunning'
-        self.notification: str = 'There is no product that matches the search criteria.'
 
     def tearDown(self) -> None:
         self.driver.quit()
 
     def test_search(self):
         """Проверка работы функции поиска"""
-        # Поиск первого продукта
+        # Поиск первого продукта "Apple"
         self.search_page.search_by_name(self.first_product_name)
         product: List[ProductInfo] = self.search_page.get_search_results()
 
+        # В результатах поиска есть искомый товар
         self.assertEqual(product[0].name, 'Apple Cinema 30"')
         self.assertEqual(product[0].price, 110.00)
 
-        # Поиск второго продукта
+        # Поиск второго продукта "Sony"
         self.search_page.clean_search_field()
         self.search_page.search_by_name(self.second_product_name)
         product: List[ProductInfo] = self.search_page.get_search_results()
 
+        # В результатах поиска есть искомый товар
         self.assertEqual(product[0].name, 'Sony VAIO')
         self.assertEqual(product[0].price, 1202.00)
 
-        # Поиск третьего продукта, которого нет на сайте
+        # Поиск третьего продукта "nokia", которого нет на сайте
         self.search_page.clean_search_field()
         self.search_page.search_by_name(self.third_product_name)
 
-        self.assertEqual(self.notification, self.search_page.get_notification())
+        # Пустая ли страница поиска?
+        self.assertTrue(self.search_page.is_page_empty())
 
         # Поиск по доп. критериям
         self.search_page.clean_search_criteria_field()
